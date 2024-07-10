3cc1806a09fbaf1447cf7929378d5f936c6b9df4
AdityaSinghBisht <adityasinghbisht1000001@gmail.com>
2024-07-10 20:35:14 +0530

    Created FrontEnd of Food-WebSite and ready to deploy

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8ienAzNABdEVzqffUQHSZGlJvqSavy9c",
+  authDomain: "foodi-website-1ea9d.firebaseapp.com",
+  projectId: "foodi-website-1ea9d",
+  storageBucket: "foodi-website-1ea9d.appspot.com",
+  messagingSenderId: "987920179190",
+  appId: "1:987920179190:web:1da773126eb2938af0b1d7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
